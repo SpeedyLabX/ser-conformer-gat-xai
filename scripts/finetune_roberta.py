5afccd0c2ab2d9c38f52a3b2122f1c9955ebe774
hei1sme <heiontheway@gmail.com>
2025-10-23 05:06:31 +0700

    feat(text): update scripts\finetune_roberta.py

--- scripts/finetune_roberta.py
+++ scripts/finetune_roberta.py
@@ -302,7 +302,7 @@ def main():
                 per_device_eval_batch_size=args.batch_size,
                 # some older HF versions don't accept evaluation_strategy kwarg; pass minimal set
                 save_strategy=args.evaluation_strategy,
-                evaluation_strategy=args.evaluation_strategy,
+                eval_strategy=args.evaluation_strategy,
                 gradient_accumulation_steps=args.accumulation_steps,
                 learning_rate=args.lr,
                 fp16=args.fp16,
